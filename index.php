35ed627b0834361ea1aeaf7246d84e4ae60e16c2
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2022-12-25 04:38:05 +0500

    bug fix

--- index.php
+++ index.php
@@ -24,11 +24,11 @@
 							<input class="timeSize border-0 pt-5" id="timePicker" type="time" name="time" />
 							<input type="date" id="datePicker" name="date" hidden/>
 						</div>
-						<?php if($record['checkout']) { ?>
+						<?php if(empty($record) || !is_null($record['checkout'])) { ?>
 							<button type="submit" class="btn btn-primary btn-lg m-4">
 								<i class="far fa-clock"></i> Clock In
 							</button>
-						<?php } else { ?>
+						<?php } elseif(is_null($record['checkout'])) { ?>
 							<button type="button" class="btn btn-primary btn-lg m-4" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
 								<i class="far fa-clock"></i> Clock Out
 							</button>
