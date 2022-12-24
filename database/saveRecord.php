4482548d7c0f5dad4c9519dfcab6e3c5a9c763a2
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2022-12-25 04:51:38 +0500

    Update saveRecord.php

--- database/saveRecord.php
+++ database/saveRecord.php
@@ -3,7 +3,7 @@ $date = $_POST["date"];
 $time = $_POST["time"];
 
 $timestamp = date("Y-m-d H:i:s", strtotime($date . ' ' . $time));
-$description = $_POST["description"];
+$description = $_POST["description"] ?? NULL;
 
 include "connectDB.php";
 
