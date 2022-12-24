b12c7b3131b67fff23767a6a5c8e29239b9bbd3b
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2022-12-24 23:30:12 +0500

    Update index.php

--- index.php
+++ index.php
@@ -31,20 +31,20 @@
 
 
 <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="staticBackdropLabel">Clock Out</h1>
-      </div>
-      <div class="modal-body">
-        Clock Out: 
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cancel</button>
-        <button type="button" class="btn btn-primary">Save</button>
-      </div>
-    </div>
-  </div>
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h1 class="modal-title fs-5" id="staticBackdropLabel">Clock Out</h1>
+			</div>
+			<form method="post" action="saveRecord.php">
+				<div class="modal-body">Clock Out:</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cancel</button>
+					<button type="button" class="btn btn-primary">Save</button>
+				</div>
+			</div>
+		</form>
+	</div>
 </div>
 
 
