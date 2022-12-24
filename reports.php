d62b29db741fce644a5f88e785d5e1ce462ef7ac
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2022-12-24 20:01:53 +0500

    Update reports.php

--- reports.php
+++ reports.php
@@ -24,23 +24,7 @@
 				</div>
 			</div>
 		</div>
-		<nav class="navbar sticky-bottom bg-light">
-			<div class="container text-center">
-				<span></span>
-				<span>
-					<a class="btn btn-link text-dark" href="index.php" role="button">
-						<i class="fas fa-home fs-3"></i>
-					</a>
-					<a class="btn btn-link text-dark" href="#" role="button">
-						<i class="fas fa-history fs-3"></i>
-					</a>
-					<a class="btn btn-link text-dark" href="reports.php" role="button">
-						<i class="fas fa-chart-pie fs-3"></i>
-					</a>
-				</span>
-				<span></span>
-			</div>
-		</nav>
+		<?php include "./components/navbar.php"; ?>
 	</body>
 </html>
 
