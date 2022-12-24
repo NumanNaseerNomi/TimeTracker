70e7e600325811c638563b21a13c128c6c2a8bd4
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2022-12-24 19:56:11 +0500

    Create navbar.php

---
+++ components/navbar.php
@@ -0,0 +1,17 @@
+<nav class="navbar sticky-bottom bg-light">
+	<div class="container text-center">
+		<span></span>
+		<span>
+			<a class="btn btn-link text-dark" href="index.php" role="button">
+				<i class="fas fa-home fs-3"></i>
+			</a>
+			<a class="btn btn-link text-dark" href="#" role="button">
+				<i class="fas fa-history fs-3"></i>
+			</a>
+			<a class="btn btn-link text-dark" href="reports.php" role="button">
+				<i class="fas fa-chart-pie fs-3"></i>
+			</a>
+		</span>
+		<span></span>
+	</div>
+</nav>
