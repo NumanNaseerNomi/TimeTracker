b5cb306c84cc93019230e4dc332c05d19c565e4a
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2022-12-24 23:07:38 +0500

    Update navbar.php

--- components/navbar.php
+++ components/navbar.php
@@ -3,10 +3,7 @@
 		<span></span>
 		<span>
 			<a class="btn btn-link text-dark" href="index.php" role="button">
-				<i class="fas fa-home fs-3"></i>
-			</a>
-			<a class="btn btn-link text-dark" href="#" role="button">
-				<i class="fas fa-history fs-3"></i>
+				<i class="fas fa-clock fs-3"></i>
 			</a>
 			<a class="btn btn-link text-dark" href="reports.php" role="button">
 				<i class="fas fa-chart-pie fs-3"></i>
