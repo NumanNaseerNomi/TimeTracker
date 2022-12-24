c2aaccdc5fca4206ca96aa4e7788f7332fffe2ac
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2022-12-24 20:45:19 +0500

    Update reports.php

--- reports.php
+++ reports.php
@@ -10,9 +10,28 @@
 	<body>
 		<div class="container text-center h-100">	
 			<div class="row">
-				<div class="col-md-6 offset-md-3">
-					<h1 class="p-5">Time Tracker</h1>
-					<h1 class="py-5"><strong>18:35</strong></h1>
+				<div class="col-md-4 offset-md-4">
+					<h1 class="p-5">Reports</h1>
+					<select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
+						<option selected>Today</option>
+						<option value="1">This Week</option>
+						<option value="2">Last Week</option>
+						<option value="3">This Year</option>
+						<option value="3">Last Year</option>
+						<option value="3">All</option>
+					</select>
+					<span class="h4">March 14, 2022</span>
+					<!-- <button type="button" class="btn btn-link">
+						<i class="far fa-clock"></i>
+					</button> -->
+
+					<div class="card m-4 mx-5">
+						<div class="card-body mx-1">
+							<p class="timeSize mb-0">44</p>
+							<span>HOURS</span>
+						</div>
+					</div>
+
 					<div class="form-check form-switch pt-5">
 						<label class="form-check-label" for="flexSwitchCheckDefault">
 							<input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault"> Manual Time
@@ -32,5 +51,10 @@
 	html,body
 	{
 		height: 100%;
+	}
+
+	.timeSize
+	{
+		font-size: 3rem;
 	}
 </style>
