e50fcc56b446d7b525fe2be275580e26b7f2f68d
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2022-12-25 18:55:12 +0500

    Create deleteRecord.php

---
+++ database/deleteRecord.php
@@ -0,0 +1,17 @@
+<?php
+$recordId = $_POST["recordId"];
+include "connectDB.php";
+
+$sql = "DELETE FROM Records WHERE id = '$recordId'";
+$query = mysqli_query($conn, $sql);
+
+if(mysqli_query($conn, $sql))
+{
+    mysqli_close($conn);
+    header('location:../reports.php');
+}
+else
+{
+    mysqli_close($conn);
+    echo("Error: " . $sql . "<br>" . mysqli_error($conn));
+}
