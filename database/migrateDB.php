f8b0b19000203f3c2c4cd14f82aaa164a5d2b2d3
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2022-12-25 21:25:01 +0500

    Create migrateDB.php

---
+++ database/migrateDB.php
@@ -0,0 +1,14 @@
+<?php
+include "connectDB.php";
+$sql = file_get_contents('TimeTracker.sql');
+
+if(mysqli_multi_query($conn, $sql))
+{
+    mysqli_close($conn);
+    echo("Database migrated successfully.");
+}
+else
+{
+    mysqli_close($conn);
+    echo("Error: " . $sql . "<br>" . mysqli_error($conn));
+}
