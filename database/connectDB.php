cfc73af840c6dea4693eed9b00657a7b6cee79b1
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2022-12-26 00:31:36 +0500

    Update connectDB.php

--- database/connectDB.php
+++ database/connectDB.php
@@ -1,20 +1,16 @@
 <?php
-	$serverName = "localhost";
-	$userName = "root";
-	$password = "";
-	$dbName = "TimeTracker";
+require_once("././env.php");
 
-	// Create connection
-	$conn = mysqli_connect($serverName, $userName, $password, $dbName);
-	$db = mysqli_select_db($conn, $dbName);
+// Create connection
+$conn = mysqli_connect($env['hostName'], $env['userName'], $env['password'], $env['dbName']);
+$db = mysqli_select_db($conn, $dbName);
 
-	// Check connection
-	if (!$conn)
-	{
-		echo("Connection failed: " . mysqli_connect_error());
-	}
-	// else
-	// {
-	// 	echo("Connected successfully");
-	// }
-?>
+// Check connection
+if (!$conn)
+{
+	echo("Connection failed: " . mysqli_connect_error());
+}
+// else
+// {
+// 	echo("Connected successfully");
+// }
