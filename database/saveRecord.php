5f820ad55882e6d2eed0177cdcf36cfc56baed7f
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2022-12-27 03:26:36 +0500

    Update saveRecord.php

--- database/saveRecord.php
+++ database/saveRecord.php
@@ -1,24 +1,21 @@
 <?php
-$date = $_POST["date"];
-$time = $_POST["time"];
-
-$timestamp = date("Y-m-d H:i:s", strtotime($date . ' ' . $time));
-$description = empty($_POST["description"]) ? '' : (', description = ' . "'" . $_POST["description"]) . "'";
-
 require_once("connectDB.php");
 
-$sql = "SELECT SQL_NO_CACHE * FROM records ORDER BY id DESC LIMIT 1";
+$timestamp = date("Y-m-d H:i:s", strtotime($_POST["timestamp"]));
+$description = empty($_POST["description"]) ? '' : (', description = ' . "'" . $_POST["description"]) . "'";
+
+$sql = "SELECT * FROM records ORDER BY id DESC LIMIT 1";
 $query = mysqli_query($conn, $sql);
 $record = mysqli_fetch_assoc($query);
 
-if(!empty($record) && is_null($record['checkout']))
+if(is_null($record) || !is_null($record['checkout']))
 {
-    $id = $record['id'];
-    $sql = "UPDATE records SET checkout = '$timestamp'" . $description . "WHERE id = '$id'";
+    $sql = "INSERT INTO records (checkin) VALUES ('$timestamp')";
 }
 else
 {
-	$sql = "INSERT INTO records (checkin) VALUES ('$timestamp')";
+    $id = $record['id'];
+    $sql = "UPDATE records SET checkout = '$timestamp'" . $description . "WHERE id = '$id'";
 }
 
 if(mysqli_query($conn, $sql))
