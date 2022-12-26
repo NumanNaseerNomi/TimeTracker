933ad21e38f9bb90cb19dc64c298a99367c4b4ec
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2022-12-27 04:05:50 +0500

    Update updateRecord.php

--- database/updateRecord.php
+++ database/updateRecord.php
@@ -1,17 +1,17 @@
 <?php
+require_once("connectDB.php");
+
 $id = $_POST["recordId"];
 $checkin = $_POST["checkinTimestamp"];
 $checkout = $_POST["checkoutTimestamp"];
 $description = empty($_POST["description"]) ? '' : (', description = ' . "'" . $_POST["description"]) . "'";
-
-require_once("connectDB.php");
 
 $sql = "UPDATE records SET checkin = '$checkin', checkout = '$checkout'" . $description . "WHERE id = '$id'";
 
 if(mysqli_query($conn, $sql))
 {
     mysqli_close($conn);
-    header('location:../reports.php');
+    header('location:../reports.php?filterBy=' . $_POST["filterBy"]);
 }
 else
 {
