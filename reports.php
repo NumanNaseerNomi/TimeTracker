deccf8a7a41b2e5a456bd929124ca31709ffade3
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2022-12-28 04:08:21 +0500

    Update reports.php

--- reports.php
+++ reports.php
@@ -81,6 +81,7 @@
 														<div class="card-body pb-0">
 															<input type="hidden" name="recordId" value="<?php echo $record['id'] ?>">
 															<input type="hidden" name="filterBy" value="<?php echo $filterBy ?>">
+															<input type="hidden" name="filterDate" />
 															<div class="row g-3">
 																<div class="col-md-6">
 																	<label for="inputEmail4" class="form-label">Clock In</label>
