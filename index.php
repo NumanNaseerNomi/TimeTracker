875f88498e6949beb7692d7f26b44cb3a4c2b6d5
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2022-12-28 23:06:00 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
-	header("Cache-Control: no-cache, must-revalidate");
-	header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
-	header("Content-Type: application/xml; charset=utf-8");
+	header("Expires: Tue, 01 Jan 2000 00:00:00 GMT");
+	header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+	header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+	header("Cache-Control: post-check=0, pre-check=0", false);
+	header("Pragma: no-cache");
 
 	require_once("./database/connectDB.php");
 
