6302b270c2a1e29623ec2ff7dd9a2d4565199b9c
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2022-12-29 00:54:38 +0500

    Update searchRecord.php

--- database/searchRecord.php
+++ database/searchRecord.php
@@ -62,6 +62,7 @@ switch($filterBy)
     {
         $fromDate = date('Y-m-d', strtotime($todayDate . "monday last week"));
         $toDate = date('Y-m-d', strtotime($todayDate . "sunday last week"));
+        $displayDateRange = date('d-m-Y', strtotime($fromDate)) . ' -- ' . date('d-m-Y', strtotime($toDate));
         $sql = "SELECT * FROM records WHERE Date(checkin) BETWEEN '$fromDate' AND '$toDate' AND checkout IS NOT NULL ORDER BY id DESC";
         break;
     }
