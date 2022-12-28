1401af0d271d139344d0d3427687afa0bab7a77e
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2022-12-28 23:14:35 +0500

    Update reports.php

--- reports.php
+++ reports.php
@@ -1,8 +1,10 @@
 <?php
-	header("Cache-Control: no-cache, must-revalidate");
-	header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
-	header("Content-Type: application/xml; charset=utf-8");
-	
+	header("Expires: Tue, 01 Jan 2000 00:00:00 GMT");
+	header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+	header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+	header("Cache-Control: post-check=0, pre-check=0", false);
+	header("Pragma: no-cache");
+
 	require_once("./database/searchRecord.php");
 	$totalSeconds = 0;
 ?>
