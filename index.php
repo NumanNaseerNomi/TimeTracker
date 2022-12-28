ce9213b179bc1ab39d7c873bfa40ff7932deac20
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2022-12-29 00:42:22 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,4 @@
 <?php
-	// header("Expires: Tue, 01 Jan 2000 00:00:00 GMT");
-	// header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
-	// header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
-	// header("Cache-Control: post-check=0, pre-check=0", false);
-	// header("Pragma: no-cache");
-
 	require_once("./database/connectDB.php");
 
 	$sql = "SELECT * FROM records WHERE checkout IS NULL ORDER BY id DESC LIMIT 1";
